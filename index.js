3b0b558057c94d99da026b74a610a6713c3a6e9d
Lynne <ilynne@gmail.com>
2020-08-24 20:27:52 -0700

    whitespace

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
